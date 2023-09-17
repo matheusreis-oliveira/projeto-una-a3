618c7fee01b0338e5f964615bad936e516f436f9
matheusreis-oliveira <mathreis1999@gmail.com>
2023-09-16 21:52:31 -0300

    feat: criação da controller generica para evitar repetição de código

--- CatalogApi/Controllers/CategoryController.cs
+++ CatalogApi/Controllers/CategoryController.cs
@@ -2,120 +2,27 @@
 using CatalogApi.DTOs;
 using CatalogApi.Models;
 using CatalogApi.Repository;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using PagedList.Pagination;
-using System.Text.Json;
 
 namespace CatalogApi.Controllers
 {
     [Route("api/[Controller]")]
     [ApiController]
-    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
-    public class CategoryController : ControllerBase
+    public class CategoryController : CommonController<Category, CategoryDTO>
     {
-        private readonly IUnitOfWork _context;
-        private readonly IMapper _mapper;
+        public CategoryController(IUnitOfWork uof, IMapper mapper)
+            : base(uof, mapper)
+        { }
 
-        public CategoryController(IUnitOfWork context, IMapper mapper)
-        {
-            _context = context;
-            _mapper = mapper;
-        }
+        protected override IRepository<Category> Repository => _uof.CategoryRepository;
 
-        [HttpGet("Products")]
+        [HttpGet("products")]
         public async Task<ActionResult<IEnumerable<CategoryDTO>>> GetProductsCategory()
         {
-            var categories = await _context.CategoryRepository
-                            .GetProductsCategory();
+            var categories = await _uof.CategoryRepository.GetProductsCategory();
 
             var categoriesDto = _mapper.Map<List<CategoryDTO>>(categories);
             return categoriesDto;
-        }
-
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<CategoryDTO>>>
-            Get([FromQuery] CategoryParameters categoriesParameters)
-        {
-            var category = await _context.CategoryRepository.
-                                GetCategory(categoriesParameters);
-
-            var metadata = new
-            {
-                category.TotalCount,
-                category.PageSize,
-                category.CurrentPage,
-                category.TotalPages,
-                category.HasNext,
-                category.HasPrevious
-            };
-
-            Response.Headers.Add("X-Pagination", JsonSerializer.Serialize(metadata));
-
-            var categoriesDto = _mapper.Map<List<CategoryDTO>>(category);
-            return categoriesDto;
-        }
-
-        [HttpGet("{id}", Name = "GetCategory")]
-        public async Task<ActionResult<CategoryDTO>> Get(Guid id)
-        {
-            var category = await _context.CategoryRepository
-                             .GetById(p => p.Id == id);
-
-            if (category == null)
-            {
-                return NotFound();
-            }
-            var categoryDto = _mapper.Map<CategoryDTO>(category);
-            return categoryDto;
-        }
-
-        [HttpPost]
-        public async Task<ActionResult> Post([FromBody] CategoryDTO categoryDto)
-        {
-            var category = _mapper.Map<Category>(categoryDto);
-
-            _context.CategoryRepository.Add(category);
-            await _context.Commit();
-
-            var categoryDTO = _mapper.Map<CategoryDTO>(category);
-
-            return new CreatedAtRouteResult("GetCategory",
-                new { id = category.Id }, categoryDTO);
-        }
-
-        [HttpPut("{id}")]
-        public async Task<ActionResult> Put(Guid id, [FromBody] CategoryDTO categoryDto)
-        {
-            if (id != categoryDto.Id)
-            {
-                return BadRequest();
-            }
-
-            var category = _mapper.Map<Category>(categoryDto);
-
-            _context.CategoryRepository.Update(category);
-            await _context.Commit();
-            return Ok();
-        }
-
-        [HttpDelete("{id}")]
-        public async Task<ActionResult<CategoryDTO>> Delete(Guid id)
-        {
-            var category = await _context.CategoryRepository
-                            .GetById(p => p.Id == id);
-
-            if (category == null)
-            {
-                return NotFound();
-            }
-            _context.CategoryRepository.Delete(category);
-            await _context.Commit();
-
-            var categoryDto = _mapper.Map<CategoryDTO>(category);
-
-            return categoryDto;
         }
     }
 }
---
+++ CatalogApi/Controllers/CommonController.cs
@@ -0,0 +1,97 @@
+﻿using AutoMapper;
+using CatalogApi.Repository;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CatalogApi.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public abstract class CommonController<TEntity, TDto> : ControllerBase
+        where TEntity : class
+        where TDto : class
+    {
+        protected readonly IUnitOfWork _uof;
+        protected readonly IMapper _mapper;
+
+        public CommonController(IUnitOfWork uof, IMapper mapper)
+        {
+            _uof = uof;
+            _mapper = mapper;
+        }
+
+        protected abstract IRepository<TEntity> Repository { get; }
+
+        [HttpGet]
+        public virtual async Task<ActionResult<IEnumerable<TDto>>> Get()
+        {
+            var entities = await Repository.Get().ToListAsync();
+            var dtos = _mapper.Map<List<TDto>>(entities);
+            return dtos;
+        }
+
+        [HttpGet("{id}")]
+        public virtual async Task<ActionResult<TDto>> Get(Guid id)
+        {
+            var entity = await Repository.GetById(e => EF.Property<Guid>(e, "Id") == id);
+
+            if (entity == null)
+            {
+                return NotFound();
+            }
+
+            var dto = _mapper.Map<TDto>(entity);
+            return dto;
+        }
+
+        [HttpPost]
+        public virtual async Task<ActionResult> Post([FromBody] TDto dto)
+        {
+            var entity = _mapper.Map<TEntity>(dto);
+
+            Repository.Add(entity);
+            await _uof.Commit();
+
+            var newDto = _mapper.Map<TDto>(entity);
+
+            return new CreatedAtRouteResult("GetById", new { id = EF.Property<Guid>(entity, "Id") }, newDto);
+        }
+
+        [HttpPut("{id}")]
+        public virtual async Task<ActionResult> Put(Guid id, [FromBody] TDto dto)
+        {
+            var entity = _mapper.Map<TEntity>(dto);
+
+            if (id != EF.Property<Guid>(entity, "Id"))
+            {
+                return BadRequest();
+            }
+
+            Repository.Update(entity);
+            await _uof.Commit();
+
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public virtual async Task<ActionResult<TDto>> Delete(Guid id)
+        {
+            var entity = await Repository.GetById(e => EF.Property<Guid>(e, "Id") == id);
+
+            if (entity == null)
+            {
+                return NotFound();
+            }
+
+            Repository.Delete(entity);
+            await _uof.Commit();
+
+            var dto = _mapper.Map<TDto>(entity);
+
+            return dto;
+        }
+    }
+}
