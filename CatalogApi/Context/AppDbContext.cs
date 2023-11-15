9c071e36e9fabda99226cfc18b0cfe2500e35f5b
Matheus Reis <mathreis1999@gmail.com>
2023-11-15 19:12:05 -0300

    azure publish

--- CatalogApi/Context/AppDbContext.cs
+++ CatalogApi/Context/AppDbContext.cs
@@ -7,11 +7,13 @@ namespace CatalogApi.Context
     public class AppDbContext : IdentityDbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-        { }
+        {
+            Database.EnsureCreated();
+        }
 
-        public DbSet<Category>? Category { get; set; }
-        public DbSet<Product>? Product { get; set; }
-        public DbSet<User>? User { get; set; }
+        public DbSet<Category> Category { get; set; }
+        public DbSet<Product> Product { get; set; }
+        public DbSet<User> User { get; set; }
 
     }
 }
